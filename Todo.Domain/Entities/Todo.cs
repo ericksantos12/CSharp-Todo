d46d216c2710b63eac6ff28c5457c327a4c132f4
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2025-10-01 02:10:09 -0300

    Add TodoCard class and update project structure
    - Introduced `TodoCard` class in `Todo.cs` with properties: `Id`, `Title`, and `IsCompleted`.
    - Updated `Todo.Domain.csproj` to include a new folder for `Enums` for better organization.

---
+++ Todo.Domain/Entities/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoCard
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+
+        public bool IsCompleted { get; private set; }
+
+    }
+}
